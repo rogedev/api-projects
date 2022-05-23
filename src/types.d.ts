dcd3dfa25817ce5800ec99a16e2e0ac672d673b7
rogedev <apteruno@gmail.com>
2022-05-22 22:40:07 -0500

    add: project interface type

---
+++ src/types.d.ts
@@ -0,0 +1,11 @@
+export interface Project {
+  id: integer
+  name: string | null
+  description: string | null
+  image: string | null
+  technologies: string[] | null
+  date: String | null
+  link: String | null
+  link: String | null
+  tags: string[] | null
+}
