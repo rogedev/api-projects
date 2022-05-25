692685e9f9a8356a1d57f71e1f5727152337937f
rogedev <apteruno@gmail.com>
2022-05-24 22:02:04 -0500

    dotnev package

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
 import express from 'express'
 import projectsRouter from './routes/projects'
+import dotenv from 'dotenv'
+dotenv.config()
 const app = express()
 
 app.use(express.json())
 
-const PORT = 3000
+const PORT = process.env.PORT ?? 3000
 
 app.get('/', (_req, res) => {
-  console.log('welcome to express')
-  res.send('welcome to express')
+  res.send('welcome to the projects api')
 })
 
 app.use('/projects', projectsRouter)
 
 app.listen(PORT, () => {
-  console.log(`server running on http://localhost:${PORT}`)
+  console.log(`server running on port ${PORT}`)
 })
